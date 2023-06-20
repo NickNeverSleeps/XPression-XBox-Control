6d0d452bed2eac83e7cf87a93d4a18ee28bee917
Nick Huntley <NHuntley96@gmail.com>
2023-06-20 14:03:55 -0700

    moving from single task to subroutine oriented code

--- Program.cs
+++ Program.cs
@@ -1,90 +1,121 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using SharpDX;
 using SharpDX.XInput;
-using System.Numerics;
-using System.Threading;
 using XPression;
 
-
-xpEngine engine = new xpEngine();
-xpSequencer sequencer = engine.Sequencer;
-
-
-while (true)
+internal class XPControl
 {
-    Controller controller = new Controller(UserIndex.One);
-    State state = controller.GetState();
-    Thread.Sleep(150);
-
-if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.A))
-{
-        xpBaseTakeItem takeItem;
-        sequencer.GetFocusedTakeItem(out takeItem);
-        takeItem.Execute();
-}
-
-    if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.B))
+    private static void Main()
     {
-        xpBaseTakeItem takeItem;
-        sequencer.GetFocusedTakeItem(out takeItem);
-        takeItem.SetOffline();
-    }
-    if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Y))
-    {
-        xpOutputFrameBuffer preview;
-        engine.GetPreviewFrameBuffer(out preview);
-        xpBaseTakeItem takeItem;
-        sequencer.GetFocusedTakeItem(out takeItem);
-        //future: run preview
-    }
-    if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadDown))
-    {
-        xpBaseTakeItem currentItem;
-        xpTakeItem intItem;
-        xpTakeItem newItem;
-        sequencer.GetFocusedTakeItem(out  currentItem);
-        newItem = (xpTakeItem)currentItem;
-        for (int i = 0; i < sequencer.ItemCount - 1; i++){
-            sequencer.GetTakeItemByIndex(i, out intItem);
-            if(intItem.ID == currentItem.ID)
-            {
-                sequencer.GetTakeItemByIndex(i+1, out newItem);
-            }
-        }
-        newItem.SetFocus();
-    }
-    if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadUp))
-    {
-        xpBaseTakeItem currentItem;
-        xpTakeItem intItem;
-        xpTakeItem newItem;
-        sequencer.GetFocusedTakeItem(out currentItem);
-        newItem = (xpTakeItem)currentItem;
-        for (int i = 0; i < sequencer.ItemCount - 1; i++)
+        while (true)
         {
-            sequencer.GetTakeItemByIndex(i, out intItem);
-            if (intItem.ID == currentItem.ID)
-            {
-                sequencer.GetTakeItemByIndex(i - 1, out newItem);
-            }
+            Controller controller = new Controller(UserIndex.One);
+            State state = controller.GetState();
+            doTasks(controller, state);
+            //Thread.Sleep(150);
         }
-        newItem.SetFocus();
     }
-    if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadLeft))
+
+    private static void doTasks(Controller controller, State state)
     {
-        xpBaseTakeItem currentItem;
-        xpBaseTakeItem newItem;
-        sequencer.GetFocusedTakeItem(out currentItem);
-        sequencer.GetTakeItemByID(currentItem.ID - 1, out newItem);
-        newItem.SetFocus();
+        xpEngine engine = new xpEngine();
+        xpSequencer sequencer = engine.Sequencer;
+        if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.A))
+        {
+            xpBaseTakeItem takeItem;
+            sequencer.GetFocusedTakeItem(out takeItem);
+            takeItem.Execute();
+            vibrateShort(controller);
+        }
+
+        if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.B))
+        {
+            xpBaseTakeItem takeItem;
+            sequencer.GetFocusedTakeItem(out takeItem);
+            takeItem.SetOffline();
+            vibrateMed(controller);
+        }
+        if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Y))
+        {
+            xpOutputFrameBuffer preview;
+            engine.GetPreviewFrameBuffer(out preview);
+            xpBaseTakeItem takeItem;
+            sequencer.GetFocusedTakeItem(out takeItem);
+            //future: run preview
+        }
+        if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadDown))
+        {
+            xpBaseTakeItem currentItem;
+            xpTakeItem intItem;
+            xpTakeItem newItem;
+            sequencer.GetFocusedTakeItem(out currentItem);
+            newItem = (xpTakeItem)currentItem;
+            for (int i = 0; i < sequencer.ItemCount - 1; i++)
+            {
+                sequencer.GetTakeItemByIndex(i, out intItem);
+                if (intItem.ID == currentItem.ID)
+                {
+                    sequencer.GetTakeItemByIndex(i + 1, out newItem);
+                }
+            }
+            newItem.SetFocus();
+        }
+        if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadUp))
+        {
+            xpBaseTakeItem currentItem;
+            xpTakeItem intItem;
+            xpTakeItem newItem;
+            sequencer.GetFocusedTakeItem(out currentItem);
+            newItem = (xpTakeItem)currentItem;
+            for (int i = 0; i < sequencer.ItemCount - 1; i++)
+            {
+                sequencer.GetTakeItemByIndex(i, out intItem);
+                if (intItem.ID == currentItem.ID)
+                {
+                    sequencer.GetTakeItemByIndex(i - 1, out newItem);
+                }
+            }
+            newItem.SetFocus();
+        }
+        if (state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadLeft))
+        {
+            xpBaseTakeItem currentItem;
+            xpBaseTakeItem newItem;
+            sequencer.GetFocusedTakeItem(out currentItem);
+            sequencer.GetTakeItemByID(currentItem.ID - 1, out newItem);
+            newItem.SetFocus();
+        }
+
+        float rightTrigger = state.Gamepad.RightTrigger;
+        if (rightTrigger != 0)
+        {
+            Console.WriteLine(rightTrigger.ToString());
+        }
+        float rightThumbstickX = state.Gamepad.RightThumbX;
     }
 
-
-    float rightTrigger = state.Gamepad.RightTrigger;
-    if(rightTrigger != 0)
+    private static void vibrateShort(Controller controller)
     {
-        Console.WriteLine(rightTrigger.ToString());
+        Vibration vibrationOn = new Vibration();
+        Vibration vibrationOff = new Vibration();
+        vibrationOn.RightMotorSpeed = 25000;
+        vibrationOn.LeftMotorSpeed = 25000;
+        vibrationOff.RightMotorSpeed = 0;
+        vibrationOff.LeftMotorSpeed = 0;
+        controller.SetVibration(vibrationOn);
+        Thread.Sleep(250);
+        controller.SetVibration(vibrationOff);
     }
-    float rightThumbstickX = state.Gamepad.RightThumbX;
 
+    private static void vibrateMed(Controller controller)
+    {
+        Vibration vibrationOn = new Vibration();
+        Vibration vibrationOff = new Vibration();
+        vibrationOn.RightMotorSpeed = 45000;
+        vibrationOn.LeftMotorSpeed = 45000;
+        vibrationOff.RightMotorSpeed = 0;
+        vibrationOff.LeftMotorSpeed = 0;
+        controller.SetVibration(vibrationOn);
+        Thread.Sleep(350);
+        controller.SetVibration(vibrationOff);
+    }
 }
